1d9d20718d853a29963a12cb1161d045d0a1b943
tidyp <tidyp1030@icloud.com>
2025-01-01 11:17:14 +0900

    chore: 추가 설정 및 코드 이동

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Refactor & Migration
+
+## 프로젝트 기간
+
+- 시작일: 2025-01-01
+- 종료일: (미정)
--- client/src/config.js
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-export const GOOGLE_REDIRECT_URI = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
-export const API_URL = import.meta.env.VITE_REQUEST_URL;
-
